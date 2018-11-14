9a44020f2d3f4e08c8fccd09cbdc1fa6220b9be9
Anna Spysz <anna.spysz@stackery.io>
2018-11-14 12:23:49 -0800

    final handler function

--- handler.js
+++ handler.js
@@ -1,8 +1,11 @@
 const crypto = require('crypto');
+const Slack = require('slack-node');
+
+// validate your payload from GitHub
 function signRequestBody(key, body) {
   return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
 }
-// The webhook handler function
+// webhook handler function
 exports.gongHandler = async event => {
   // get the GitHub secret from the environment variables
   const token = process.env.GITHUB_WEBHOOK_SECRET;
@@ -13,13 +16,17 @@ exports.gongHandler = async event => {
   const sig = headers['X-Hub-Signature'];
   const githubEvent = headers['X-GitHub-Event'];
   const body = JSON.parse(event.body);
-  // this determines username for a push event, but lists the repo owner for other events
-  const username = body.pusher ? body.pusher.name : body.repository.owner.login;
-  const message = body.pusher ? `${username} pushed this awesomeness/atrocity through (delete as necessary)` : `The repo owner is ${username}.`
   // get repo variables
-  const { repository } = body;
+  const { repository, release } = body;
   const repo = repository.full_name;
   const url = repository.url;
+  // set variables for a release event
+  let releaseVersion, releaseUrl, author = null;
+  if (githubEvent === 'release') {
+    releaseVersion = release.tag_name;
+    releaseUrl = release.html_url;
+    author = release.author.login;
+  }
   
   // check that a GitHub webhook secret variable exists, if not, return an error
   if (typeof token !== 'string') {
@@ -40,10 +47,31 @@ exports.gongHandler = async event => {
     };
   }
 
-  // print some messages to the CloudWatch console
+  // if the event is a 'release' event, gong the Slack channel!
+  const webhookUri = process.env.SLACK_WEBHOOK_URL;
+
+  const slack = new Slack();
+  slack.setWebhook(webhookUri);
+
+  // send slack message
+  if (githubEvent === 'release') {
+    slack.webhook({
+      channel: "#gong-test", // your desired channel here
+      username: "gongbot",
+      icon_emoji: ":gong:", // because Slack is for emojis
+      text: `It's time to celebrate! ${author} pushed release version ${releaseVersion}. See it here: ${releaseUrl}!\n:gong:  https://youtu.be/8nBOF5sJrSE?t=11` // your message
+    }, function(err, response) {
+      console.log(response);
+      if (err) {
+        console.log('Something went wrong');
+        console.log(err);
+      }
+    });
+  }
+
+  // (optional) print some messages to the CloudWatch console (for testing)
   console.log('---------------------------------');
-  console.log(`\nGithub-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.\n ${message}`);
-  console.log('Contents of event.body below:');
+  console.log(`\nGithub-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.`);
   console.log(event.body);
   console.log('---------------------------------');
   
